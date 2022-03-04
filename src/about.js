dcb6a60378c9f2cdd137ef723e9e30ae220192f3
nnekasandra <nnekasandra@gmail.com>
2022-03-04 09:27:58 +0100

    removed darkMode option in tailwind config file

--- src/about.js
+++ src/about.js
@@ -6,7 +6,7 @@ function About(){
     return(
         <section className="flex">
             <div className="about">
-                <p>EVERY CHILD YEARNS TO LEARN</p>
+                <p>EVERY CHILD YEARNS TO LEARN WELL</p>
                 <h1>Making Your Childs World Better</h1>
                 <p>Replenish seasons may male hath fruit beast were seas saw you arrie said man beast whales his void unto last session for bite. Set have great you'll male grass yielding yielding man</p>
                 <div className="flex">
